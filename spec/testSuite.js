642158934dad313ec28e3ae58742ad1614344b29
Luke Phyall <lukeprospect@yahoo.com>
2019-03-18 14:43:27 +0000

    Adds an actual test.

--- spec/testSuite.js
+++ spec/testSuite.js
@@ -1,7 +1,22 @@
 var assert = {
+
   isTrue: function(assertionToCheck) {
     if (!assertionToCheck) {
       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
     }
-  };
+  },
+
+  isEqual: function(assertionToCheck, expectedValue){
+    if (assertionToCheck == expectedValue) {
+      console.log(`${expectedValue} is equal to ${assertionToCheck}. Congratulations.`)
+    } else {
+      console.log((`${expectedValue} is NOT equal to ${assertionToCheck}. Comiserations :-(.`))
+    }
+  }
+
+
 };
+
+assert.isEqual(2,2);
+
+assert.isEqual("Two",2);
