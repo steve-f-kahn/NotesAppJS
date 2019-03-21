d14963433ce264bbd88cbb46515d2a46aab86310
James Ault <james.ault@johnlewis.co.uk>
2019-03-21 16:20:35 +0000

    Passing test for id being added to notes during notelist addNote

--- spec/noteListModelSpec.js
+++ spec/noteListModelSpec.js
@@ -1,7 +1,32 @@
 var noteList = new NoteList();
+var noteList2 = new NoteList();
+var noteList3 = new NoteList();
+var noteList4 = new NoteList();
 
+function NoteDouble() {
+  this.text = 'test note'
+};
 
 it("check to see if 'I am a Note!' is actually in there", function() {
   noteList.addNote("This is a new one")
   assert.includedInArray(noteList.notes(),'This is a new one')
 });
+
+it('increments the id number when adding a note to the list', function(){
+  noteDouble = new NoteDouble
+  noteList2.addNote(noteDouble)
+  assert.isEqual(noteList2.n, 1)
+})
+
+it('further increments the id number when adding a note to the list', function(){
+for (i=0;i<5;i++){
+  noteList3.addNote(noteDouble)
+}
+  assert.isEqual(noteList3.n, 5)
+})
+
+it('the note is given an id after being added to the list', function(){
+  noteDouble = new NoteDouble
+  noteList4.addNote(noteDouble)
+  assert.isEqual(noteDouble.id, 0)
+})
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -3,7 +3,7 @@
   function NoteList() {
 
     this._notes = [];
-
+    this.n = 0
   }
 
   NoteList.prototype.notes = function() {
@@ -11,7 +11,9 @@
   }
 
   NoteList.prototype.addNote = function (text) {
+    text.id = this.n
     this._notes.push(text)
+    this.n += 1
   };
 
   exports.NoteList = NoteList;
